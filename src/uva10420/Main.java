52b95a15155ec884f22c9bf6afe5ac142cffa2ca
A. H. M. Imrul Hasan <imrul001@gmail.com>
2017-04-03 13:55:36 +0700

    Accepted UVA10420..
    Signed-off-by: A. H. M. Imrul Hasan <imrul001@gmail.com>

--- src/uva10420/Main.java
+++ src/uva10420/Main.java
@@ -1,9 +1,10 @@
 package uva10420;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -12,17 +13,37 @@ public class Main {
 		myWork.begin();
 	}
 
-	private void begin() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(
-				"F:\\imrul\\workspace\\UVA\\src\\uva10420\\input.txt"));
+	void begin() throws IOException {
+		// BufferedReader reader = new BufferedReader(new FileReader(
+		// "/home/linux/tutorial/UVA/src/uva10420/input.txt"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 		String line;
-		List<String> contries = new ArrayList<String>();
+		List<String> countries = new ArrayList<String>();
 		while ((line = reader.readLine()) != null) {
 			int numberofLine = Integer.valueOf(line);
 			for (int i = 0; i < numberofLine; i++) {
-				contries.add(reader.readLine().split(" ")[0]);
+				countries.add(reader.readLine().split(" ")[0]);
 			}
-			// Collections.sort(contries);
+			Collections.sort(countries);
+			getCount(countries);
 		}
+	}
+
+	void getCount(List<String> countries) {
+		List<String> temp = new ArrayList<String>();
+		temp.add(countries.get(0));
+		int count = 0;
+		for (String country : countries) {
+			if (country.equals(temp.get(0))) {
+				count++;
+			} else {
+				System.out.println(temp.get(0) + " " + count);
+				count = 1;
+				temp.remove(0);
+				temp.add(country);
+			}
+		}
+		System.out.println(temp.get(0) + " " + count);
 	}
 }
