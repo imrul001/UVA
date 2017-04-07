bc5f7a75a56fe7afa09357c43e1c4e8951cd5c30
imrul001 <imrul001@gmail.com>
2017-04-07 18:02:34 +0700

    Basic Structure of uva400
    Signed-off-by: imrul001 <imrul001@gmail.com>

---
+++ src/uva400/Main.java
@@ -0,0 +1,105 @@
+package uva400;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Main myWork = new Main();
+		myWork.begin();
+	}
+
+	private void begin() throws IOException {
+		// BufferedReader reader = new BufferedReader(new FileReader(
+		// "F:\\imrul\\workspace\\UVA\\src\\uva400\\input.txt"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			int numberOfFiles = Integer.parseInt(line);
+			List<MyString> fileNames = new ArrayList<MyString>();
+			int maxNameLength = 0;
+			for (int i = 0; i < numberOfFiles; i++) {
+				String fileName = reader.readLine();
+				int temp = fileName.length();
+				if (temp > maxNameLength) {
+					maxNameLength = temp;
+				}
+				fileNames.add(new MyString(fileName));
+			}
+			solve(fileNames, maxNameLength);
+			fileNames.removeAll(fileNames);
+		}
+	}
+
+	private void solve(List<MyString> fileNames, int maxNameLength) {
+		// maximum number of possible columns
+		int maxNumber0fColumns = ((60) / (maxNameLength + 2));
+		float numberOfFiles = (float) fileNames.size();
+		int row = 0, column = 0;
+		for (float i = 1;; i++) {
+			float temp = numberOfFiles / i;
+			if (temp <= maxNumber0fColumns) {
+				column = (int) temp;
+				row = (int) i;
+				break;
+			}
+		}
+		Collections.sort(fileNames);
+		printResult(fileNames, row, column, maxNameLength + 2);
+	}
+
+	void printResult(List<MyString> fileNames, int row, int column,
+			int maxLength) {
+		for (int i = 1; i <= 60; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+		for (int i = 0; i < row; i++) {
+			List<String> r = new ArrayList<String>();
+			String str1 = fileNames.get(i).getFileName();
+			r.add(str1 + printSpace(maxLength - str1.length()));
+			int index = i + row;
+			for (int j = 0; j < column; j++) {
+				if (index >= fileNames.size()) {
+					break;
+				}
+				String str2 = fileNames.get(index).getFileName();
+				r.add(str2 + printSpace(maxLength - str2.length()));
+				index = index + row;
+			}
+			for (String fileName : r) {
+				System.out.print(fileName);
+			}
+			System.out.println();
+		}
+	}
+
+	static String printSpace(int size) {
+		String str = "";
+		for (int i = 0; i < size; i++) {
+			str = str + " ";
+		}
+		return str;
+	}
+}
+
+class MyString implements Comparable<MyString> {
+	private String fileName;
+
+	public MyString(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public int compareTo(MyString object) {
+		return this.fileName.compareTo(object.fileName);
+	}
+}
