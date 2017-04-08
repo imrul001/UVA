5d2719d796d1169a7356debfed9240e44edec1d1
imrul001 <imrul001@gmail.com>
2017-04-09 01:56:09 +0700

    Accepted UVA400
    One should be careful while formatting the file names..Otherwise fairly
    simple problem
    
    
    Signed-off-by: imrul001 <imrul001@gmail.com>

--- src/uva400/Main.java
+++ src/uva400/Main.java
@@ -1,8 +1,8 @@
 package uva400;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,11 +13,11 @@ public class Main {
 		myWork.begin();
 	}
 
-	private void begin() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(
-				"F:\\imrul\\workspace\\UVA\\src\\uva400\\input.txt"));
-		// BufferedReader reader = new BufferedReader(new InputStreamReader(
-		// System.in));
+	void begin() throws IOException {
+		// BufferedReader reader = new BufferedReader(new FileReader(
+		// "F:\\imrul\\workspace\\UVA\\src\\uva400\\input.txt"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 		String line;
 		while ((line = reader.readLine()) != null) {
 			int numberOfFiles = Integer.parseInt(line);
@@ -32,40 +32,36 @@ public class Main {
 				fileNames.add(new MyString(fileName));
 			}
 			Collections.sort(fileNames);
-			solveRewrite(fileNames, maxNameLength);
+			solve(fileNames, maxNameLength);
 		}
 	}
 
-	private void solveRewrite(List<MyString> fileNames, int maxNameLength) {
-		// maximum number of possible columns
-		float maxNumber0fColumns = ((60) / (maxNameLength + 2));
-		float numberOfFiles = (float) fileNames.size();
+	void solve(List<MyString> fileNames, int maxNameLength) {
+		int maxNumber0fColumns = (62 / (maxNameLength + 2));
+		int numberOfFiles = fileNames.size();
 		int row = 0, column = 0;
-		float temp = numberOfFiles / maxNumber0fColumns;
 		if (numberOfFiles % maxNumber0fColumns != 0) {
-			temp = temp + 1;
-		}
-		row = (int) temp;
-		if (numberOfFiles % row != 0) {
-			column = (int) (numberOfFiles / row) + 1;
+			row = (numberOfFiles / maxNumber0fColumns) + 1;
 		} else {
-			column = (int) (numberOfFiles / row);
+			row = numberOfFiles / maxNumber0fColumns;
+		}
+		if (numberOfFiles % row != 0) {
+			column = (numberOfFiles / row) + 1;
+		} else {
+			column = (numberOfFiles / row);
 		}
 		printResult(fileNames, row, column, maxNameLength + 2);
 	}
 
 	void printResult(List<MyString> fileNames, int row, int column,
 			int maxLength) {
-		System.out.println(fileNames.size() + " " + row + " " + column + " "
-				+ maxLength);
 		System.out
 				.println("------------------------------------------------------------");
 		int count = 0;
 		for (int i = 0; i < row; i++) {
 			count++;
 			String str1 = fileNames.get(i).getFileName();
-			System.out.print(str1 + " " + i
-					+ printSpace(maxLength - str1.length()));
+			System.out.print(str1 + printSpace(maxLength - str1.length()));
 			int index = i + row;
 			for (int j = 0; j < column - 1; j++) {
 				count++;
@@ -74,10 +70,10 @@ public class Main {
 				}
 				String str2 = fileNames.get(index).getFileName();
 				if (count == column) {
-					System.out.print(str2 + " " + index
+					System.out.print(str2
 							+ printSpace((maxLength - 2) - str2.length()));
 				} else {
-					System.out.print(str2 + " " + index
+					System.out.print(str2
 							+ printSpace(maxLength - str2.length()));
 				}
 				index = index + row;
@@ -87,40 +83,9 @@ public class Main {
 		}
 	}
 
-	void printResultRewrite(List<MyString> fileNames, int row, int column,
-			int maxLength) {
-		System.out
-				.println("------------------------------------------------------------");
-		int columnCount = 0;
-		for (int i = 0; i < row; i++) {
-			columnCount++;
-			String name1 = fileNames.get(i).getFileName();
-			System.out.print(name1 + printSpace(maxLength - name1.length()));
-			int index = i + row;
-			for (int j = 0; j < column - 1; j++) {
-				columnCount++;
-				if (index >= fileNames.size()) {
-					break;
-				}
-				String name2 = fileNames.get(index).getFileName();
-				if (columnCount == column) {
-					System.out.print(name2
-							+ printSpace((maxLength - 2) - name2.length()));
-				} else {
-					System.out.print(name2
-							+ printSpace(maxLength - name2.length()));
-				}
-				index = index + row;
-				columnCount = 0;
-			}
-			System.out.println();
-		}
-	}
-
 	static String printSpace(int size) {
 		return new String(new char[size]).replace('\0', ' ');
 	}
-
 }
 
 class MyString implements Comparable<MyString> {
