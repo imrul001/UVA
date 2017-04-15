c1f19b82543c07abc7c4e5183071a131dd97811c
imrul001 <imrul001@gmail.com>
2017-04-15 14:33:25 +0700

    Accepted UVA10193
    Nice problem: Trick is to know how to calculate greatest Common divisor(GCD)
    between two numbers. Enjoyed !!
    
    Signed-off-by: imrul001 <imrul001@gmail.com>

--- src/uva10193/Main.java
+++ src/uva10193/Main.java
@@ -1,8 +1,8 @@
 package uva10193;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,10 +11,10 @@ public class Main {
 	}
 
 	private void begin() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(
-				"F:\\imrul\\workspace\\UVA\\src\\uva10193\\input.txt"));
-		// BufferedReader reader = new BufferedReader(new InputStreamReader(
-		// System.in));
+		// BufferedReader reader = new BufferedReader(new FileReader(
+		// "F:\\imrul\\workspace\\UVA\\src\\uva10193\\input.txt"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 		int testCases = Integer.parseInt(reader.readLine());
 		for (int i = 1; i <= testCases; i++) {
 			String s1 = reader.readLine();
@@ -27,19 +27,13 @@ public class Main {
 	private static void solve(String s1, String s2, int pair) {
 		int num1 = getInt(s1);
 		int num2 = getInt(s2);
-		boolean isLove = false;
-
-		int limit = num1 > num2 ? num2 : num1;
-		for (int i = limit; i >= 2; i--) {
-			if ((num1 % i == 0)) {
-				if ((num2 % i == 0)) {
-					System.out.println("divisor " + i);
-					isLove = true;
-					break;
-				}
-			}
+		int r;
+		while (num2 != 0) {
+			r = num1 % num2;
+			num1 = num2;
+			num2 = r;
 		}
-		if (isLove) {
+		if (num1 != 1) {
 			System.out.println("Pair #" + pair + ": All you need is love!");
 		} else {
 			System.out.println("Pair #" + pair + ": Love is not all you need!");
@@ -54,16 +48,5 @@ public class Main {
 			}
 		}
 		return integer;
-	}
-
-	public static boolean isPrime(long n) {
-		// fast even test.
-		if (n > 2 && (n & 1) == 0)
-			return false;
-		// only odd factors need to be tested up to n^0.5
-		for (int i = 3; i * i <= n; i += 2)
-			if (n % i == 0)
-				return false;
-		return true;
 	}
 }
